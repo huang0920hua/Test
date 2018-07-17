a3a2fe74c968eee2c610070484fe9d94155618bc
huangzh <hz824581849@qq.com>
2018-07-17 09:29:34 +0800

    commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by careland on 2018/7/17.
+//  Copyright © 2018年 careland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
